4aacba00ca8bd8c88d7e71fc67d07dd01f41df81
Souwg <sansanvzla@gmail.com>
2025-01-25 22:13:53 +0000

    autentication JWT

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,10 +1,17 @@
+
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, BlockedTokenList
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, get_jwt
+
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +27,66 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+{
+    "email": "pingu@pingu.com",
+    "password": "123456"
+}
+
+@api.route('/signup', methods=['POST'])
+def register_user():
+    body=request.get_json()
+    # Make routine verifications
+    email = body.get('email')
+    password = body.get('password')
+    user_exists = User.query.filter_by(email=email).first()
+    if user_exists:
+        return jsonify({"msg": "Email already registered"})
+    if not email:
+        return jsonify({'msg': "Need Email"}),400
+    if not password:
+        return jsonify({'msg': "Need Password"}),400
+    # Encrypt password
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    user = User(email=email, password=hashed_password, is_active=True)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"msg":"Usuario registrado Exitosamente"})
+
+@api.route('/login', methods=['POST'])
+def login_user():
+    body=request.get_json()
+    email = body.get('email')
+    password = body.get('password')
+    if not email:
+        return jsonify({'msg': "Need Email"}),400
+    if not password:
+        return jsonify({'msg': "Need Password"}),400
+    
+    user= User.query.filter_by(email=email).first()
+
+    if not user:
+        return jsonify({"msg": "User not Found"}),404
+    
+    valid_password = bcrypt.check_password_hash(user.password, password)
+    if not valid_password:
+        return jsonify({"msg": "Invalid Credentials"})
+    
+    acces_token = create_access_token(identity=user.id)
+    return jsonify({"token": acces_token})
+
+@api.route('/userinfo', methods=['GET'])
+@jwt_required()
+def private_info():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    return jsonify({"userInfo":user.serialize()}),200
+
+@api.route('/logout', methods=['POST'])
+@jwt_required()
+def user_logout():
+    token = get_jwt()
+    blocked_token = BlockedTokenList(jti=token["jti"])
+    db.session.add(blocked_token)
+    db.session.commit()
+    return jsonify({"msg":"Session cerrada"})
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 export const Navbar = () => {
+	const navigate = useNavigate()
+
+	const handleLogOut = async () =>{
+		let token = window.sessionStorage.getItem('token') 
+		if(token){
+			let res = await fetch("https://cuddly-trout-5gq7w6vjvxxw3vr5r-3001.app.github.dev/api/logout",{
+				method: 'POST',
+				headers:{
+					"Authorization": `Bearer ${token}`,
+					"Content-Type": "application/json"
+				}
+			})
+			if (res.ok){
+				let response = await res.json()
+				console.log(response)
+				window.sessionStorage.removeItem("token")
+				navigate("/")
+			}
+		}
+	}
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Home</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					{window.sessionStorage.getItem('token')? (<button onClick={() => handleLogOut()} className="btn btn-danger">LogOut</button>): 					<Link to="/signup">
+						<button className="btn btn-primary">Signup</button>
+					</Link>}
+
 				</div>
 			</div>
 		</nav>
